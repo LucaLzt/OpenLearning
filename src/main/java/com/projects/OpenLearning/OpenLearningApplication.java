79949c099cda5a2370d70918d1c98d9eb54d25b1
LucaLzt <luca.lazarte05@gmail.com>
2026-01-25 14:56:15 -0300

    initial commit

---
+++ src/main/java/com/projects/OpenLearning/OpenLearningApplication.java
@@ -0,0 +1,13 @@
+package com.projects.OpenLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/OpenLearning/OpenLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.OpenLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
