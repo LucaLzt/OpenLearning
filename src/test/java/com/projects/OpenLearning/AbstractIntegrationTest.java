ec71b971f79ee5ebd16b5a9c5d079c2c19723a62
Luca Lazarte <luca.lazarte05@gmail.com>
2026-02-07 14:15:30 -0300

    feat(test): add AbstractIntegrationTest to establish test context
    - add TestContainers dependencies
    - configure MinIO, PostgreSQL and Redis containers
    - configure dynamic environment variables within AbstractIntegrationTest
    - modify OpenLearningApplicationTest to extend AbstractIntegrationTest

---
+++ src/test/java/com/projects/OpenLearning/AbstractIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.projects.OpenLearning;
+
+import com.redis.testcontainers.RedisContainer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MinIOContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AbstractIntegrationTest {
+
+    // 1. We define a static containers to be shared across all test classes.
+    // This way we avoid the overhead of starting a new container for each test class.
+    static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:17-alpine");
+    static final RedisContainer redis = new RedisContainer("redis:7.4-alpine");
+    static final MinIOContainer minio = new MinIOContainer("minio/minio:RELEASE.2025-09-07T16-13-09Z-cpuv1");
+
+    static {
+        // 2. Start the containers before any tests run.
+        postgres.start();
+        redis.start();
+        minio.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        // 3. Override application properties to point to the containers.
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", redis::getFirstMappedPort);
+
+        registry.add("app.aws.s3.endpoint-url", minio::getS3URL);
+        registry.add("app.aws.s3.access-key", () -> "admin");
+        registry.add("app.aws.s3.secret-key", () -> "password");
+    }
+}
--- src/test/java/com/projects/OpenLearning/OpenLearningApplicationTests.java
+++ src/test/java/com/projects/OpenLearning/OpenLearningApplicationTests.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OpenLearningApplicationTests {
+class OpenLearningApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
+		// 1. Levanta el contexto de Spring
+		// 2. Levanta Postgres en Docker
+		// 3. Levanta Redis en Docker
+		// 4. Levanta MinIO en Docker
+		// 5. La aplicación se conectó a los tres servicios
 	}
 
 }
